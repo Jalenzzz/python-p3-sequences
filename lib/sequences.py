2c50d1d2d2e396877786fd87c43942d99e691f2a
Jalen Mnene <mnenejalen72@gmail.com>
2024-05-26 23:59:55 +0300

    Finished instructions for sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
+# prints empty list when length is 0
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+
+    fibonacci_sequence = [0, 1]
+    for i in range(2, length):
+        next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_value)
+
+    print(fibonacci_sequence[:length])
+
+# print the first 10 numbers
+print_fibonacci(10)
